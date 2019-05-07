cfd637f5b00d2e79579fdd99a31dce3b8a05f536
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-05-07 14:21:13 -0500

    add router update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import * as React from 'react';
+import { withRouter } from 'next/router';
 import * as R from 'ramda';
 import Header from '../components/header';
 import { OrbsList } from '../components/OrbsList';
@@ -8,11 +10,22 @@ const ONE_MILLION = 100000;
 const orbs = R.range(1, ONE_MILLION);
 
 class Index extends React.Component {
+  setQuery = e => {
+    // const searchInput = e.target.value;
+    const searchInput = 'test';
+
+    this.props.router.replace('/', {
+      pathname: '/',
+      query: { q: searchInput }
+    });
+  };
+
   render() {
     return (
       <main>
         <Header />
         <section>
+          <button onClick={this.setQuery}>Update search</button>
           <OrbsList orbs={orbs} />
         </section>
       </main>
@@ -20,4 +33,4 @@ class Index extends React.Component {
   }
 }
 
-export default Index;
+export default withRouter(Index);
