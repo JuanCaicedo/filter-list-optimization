dcfb6c010a29008cafc6d0df8657c557e2a1e96e
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-05-09 10:30:10 -0500

    assertions on text box

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -4,10 +4,12 @@ describe('Numbers list page', () => {
   it('with no initial search query', () => {
     cy.visit(baseUrl);
     cy.get('.number-row').should('have.length', 9999);
+    cy.get('.search-box').should('have.value', '');
   });
 
   it('with initial search query', () => {
     cy.visit(`${baseUrl}?q=9999`);
     cy.get('.number-row').should('have.length', 1);
+    cy.get('.search-box').should('have.value', '9999');
   });
 });
