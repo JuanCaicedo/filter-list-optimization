0e938c7f594bf4ad1e1837fee71e89e1b1781bb0
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-05-09 10:23:43 -0500

    test initial search query

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,8 +1,13 @@
 const baseUrl = 'http://localhost:3000/';
 
 describe('Numbers list page', () => {
-  it('renders all numbers on page load', () => {
+  it('with no initial search query', () => {
     cy.visit(baseUrl);
     cy.get('.number-row').should('have.length', 9999);
+  });
+
+  it('with initial search query', () => {
+    cy.visit(`${baseUrl}?q=9999`);
+    cy.get('.number-row').should('have.length', 1);
   });
 });
