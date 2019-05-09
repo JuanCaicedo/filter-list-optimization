6975cc24c216715163dcb839ddfcee3ae2d6a6f1
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-05-09 10:33:57 -0500

    test search box behavior

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -8,8 +8,24 @@ describe('Numbers list page', () => {
   });
 
   it('with initial search query', () => {
-    cy.visit(`${baseUrl}?q=9999`);
+    cy.visit(`${baseUrl}?q=9991`);
     cy.get('.number-row').should('have.length', 1);
-    cy.get('.search-box').should('have.value', '9999');
+    cy.get('.search-box').should('have.value', '9991');
+  });
+
+  it('filters on typing', () => {
+    cy.visit(`${baseUrl}?q=999`);
+    cy.get('.search-box')
+      .type('1')
+      .should('have.value', '9991');
+    cy.get('.number-row').should('have.length', 1);
+  });
+
+  it('removes filters when search cleared', () => {
+    cy.visit(`${baseUrl}?q=999`);
+    cy.get('.search-box')
+      .clear()
+      .should('have.value', '');
+    cy.get('.number-row').should('have.length', 9999);
   });
 });
