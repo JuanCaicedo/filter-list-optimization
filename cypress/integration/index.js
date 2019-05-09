c142aa885971b67e094a58ebd3037eca7e0da473
Juan Caicedo <retiredcanadianpoet@gmail.com>
2019-05-09 11:48:50 -0500

    rename tests

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,13 +1,13 @@
 const baseUrl = 'http://localhost:3000/';
 
 describe('Numbers list page', () => {
-  it('with no initial search query', () => {
+  it('loads all number with no initial search query', () => {
     cy.visit(baseUrl);
     cy.get('.number-row').should('have.length', 9999);
     cy.get('.search-box').should('have.value', '');
   });
 
-  it('with initial search query', () => {
+  it('loads filtered numbers with initial search query', () => {
     cy.visit(`${baseUrl}?q=9991`);
     cy.get('.number-row').should('have.length', 1);
     cy.get('.search-box').should('have.value', '9991');
